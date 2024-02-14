c6a6f08de6023b7f98a2a4f2a4725d6e429f0700
ViniHGV <ixxvinicius@gmail.com>
2024-02-14 19:56:52 -0300

    feat: Fazendo injeção de depêndecias por meio de interface do serviço corrigindo detalhes dos endpoints e adicionando os endpoints de atualizar e deletar Categories

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -6,9 +6,9 @@ namespace BookHub.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class CategoryController(CategoryService _categoryService) : ControllerBase
+    public class CategoryController(ICategoryService categoryService) : ControllerBase
     {
-        private readonly CategoryService _categoryService = _categoryService;
+        private readonly ICategoryService _categoryService = categoryService;
 
         [HttpGet]
         public async Task<IActionResult> GetAllCategories([FromQuery] int pageSkip)
@@ -19,9 +19,9 @@ namespace BookHub.API.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateCategory([FromBody] CreateCategoryDTO categoryDTO)
         {
-            await _categoryService.CreateCategory(categoryDTO);
             try
             {
+                await _categoryService.CreateCategory(categoryDTO);
                 return Ok($"Categoria {categoryDTO.Name} criada com sucesso!");
             }
             catch (Exception e)
@@ -34,11 +34,41 @@ namespace BookHub.API.Controllers
         [Route("{id}")]
         public async Task<IActionResult> GetCategoryById([FromRoute] int id, [FromQuery] int pageSkip)
         {
-            var categoryById = await _categoryService.GetCategoryById(id, pageSkip);
-
             try
             {
+                var categoryById = await _categoryService.GetCategoryById(id, pageSkip);
                 return Ok(categoryById);
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> UpdateCategory([FromRoute] int id,
+         [FromBody] CreateCategoryDTO entityDTO)
+        {
+            try
+            {
+                await _categoryService.UpdateCategory(id, entityDTO);
+                return Ok("Categoria Editada com sucesso!");
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteCategory([FromRoute] int id)
+        {
+            try
+            {
+                await _categoryService.DeleteCategory(id);
+                return Ok("Categoria Editada com sucesso!");
             }
             catch (Exception e)
             {
